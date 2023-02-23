40fcfc57d919c50ec738db70cbe0543f46ab5cd3
mdshowkatislam <mdshowkatislam8876643@yahoo.com>
2023-02-23 19:24:54 +0600

    A registration form api create and tested through postman

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -2,14 +2,49 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Auth\Events\Validated;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 class UserController extends Controller
 {
     public function UserRegister(Request $req)
     {
-        dd($req->all());
+        $val_data = Validator::make($req->all(), [
+            'first_name' => 'required|string|max:20',
+            'last_name' => 'required|string|max:20',
+            'email' => 'required|string|email|unique:users',
+            'phone' => 'required|string|unique:users|digits:8',
+            'password' => 'required|string|min:8|confirmed',
+            // 'password_confirmation' => 'required|min:8',
+        ]);
+        if ($val_data->fails()) {
+            $res_sms = [
+                'status' => 'Unsuccessed',
+                'message' => 'validation failed for you',
+                'validation-errors' => $val_data->errors()->all(),
+            ];
+            return response($res_sms, 422);
+        }
+        $data = $req->all();
+        $data['password'] = Hash::make($req['password']);
+        $user = User::create($data);
+        if ($user) {
+            $res_sms = [
+                'status' => 'succesfull',
+                'message' => 'user imported succesfully',
+                'data' => $user,
+            ];
+            return response($res_sms, 200);
+        } else {
+            return response()->json([
+                'status' => 'succesfull',
+                'message' => 'user imported succesfully',
+            ]);
+        }
     }
     public function UserLogin(Request $req)
     {
