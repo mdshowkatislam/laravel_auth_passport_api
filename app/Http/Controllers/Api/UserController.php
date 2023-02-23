741d0a3c0f02f2721c35e1b46c59ace0464aff0b
mdshowkatislam <mdshowkatislam8876643@yahoo.com>
2023-02-23 19:56:21 +0600

    Creating Login method in UserController

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -2,12 +2,13 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Auth\Events\Validated;
 use Illuminate\Support\Facades\Validator;
-use Illuminate\Support\Facades\Hash;
-use App\Models\User;
 
 class UserController extends Controller
 {
@@ -48,7 +49,33 @@ class UserController extends Controller
     }
     public function UserLogin(Request $req)
     {
-        dd($req->all());
+        $val_data = Validator::make($req->all, [
+            'first_name' => 'required|string|max:20',
+            'last_name' => 'required|string|max:20',
+            'email' => 'required|string|email',
+            'phone' => 'required|string|digits:8',
+            'password' => 'required|string|min:8',
+        ]);
+        if ($val_data->fails()) {
+            $response = [
+                'status' => 'Login Unsuccess !',
+                'message' => 'Login information is wrong !',
+                'validation-errors' => $val_data->errors(),
+            ];
+            return response($response, 422);
+        }
+        // Checking email & passwor credentials
+
+        if (
+            Auth::attempt([
+                'email' => $req->email,
+                'password' => $req->password,
+            ])
+        ) {
+            $user = Auth::User();
+            dd($user);
+            $tokengen = $user->createToken('my_token')->accessToken();
+        }
     }
     public function UserLogout(Request $req)
     {
